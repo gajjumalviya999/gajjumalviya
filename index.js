f5dbd586aa4a480419a76d9d2b3370a6fbbbf3ab
gajjumalviya999@gmail.com <gajjumalviya999@gmail.com>
2021-05-25 14:54:34 +0530

    Added typing effect in name

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let i=1;
 let txt="Gajendra Malviya";
+let typingcur="|";
 let nameTypeId=document.querySelectorAll(".nameType");
 function typingEffect(){
-    if(i<txt.length){
-        nameTypeId[0].innerText= txt.slice(0,i+1);
-        nameTypeId[1].innerText= txt.slice(0,i+1);
-        
+    if(i<txt.length-1){
+        nameTypeId[0].innerText= txt.slice(0,i+1)+typingcur;
+        nameTypeId[1].innerText= txt.slice(0,i+1)+typingcur;
         i++;
         setTimeout(typingEffect,50);
+    }
+    if(i==txt.length-1){
+        nameTypeId[0].innerText= txt.slice(0,i+1);
+        nameTypeId[1].innerText= txt.slice(0,i+1);
     }
 }
 typingEffect();
